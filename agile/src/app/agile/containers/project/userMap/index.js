6119db557566bc3bb6dec17df71cd14edf08d327
12456 <handhand>
2018-08-08 09:39:01 +0800

    [IMP]添加用户故事store

--- agile/src/app/agile/containers/project/userMap/index.js
+++ agile/src/app/agile/containers/project/userMap/index.js
@@ -5,7 +5,7 @@ import {
 } from 'react-router-dom';
 import { asyncRouter, nomatch } from 'choerodon-front-boot';
 
-const UserMapHome = asyncRouter(() => (import('./home')));
+const UserMapHome = asyncRouter(() => (import('./home')), () => (import('../../../stores/project/userMap/UserMapStore')));
 
 const UserMapIndex = ({ match }) => (
   <Switch>
