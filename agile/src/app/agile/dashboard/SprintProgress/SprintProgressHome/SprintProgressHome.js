4985c65e6845bf0fbac2a25c364e63fa8540bea1
20615 <wenfei.li@hand-china.com>
2018-08-28 16:26:32 +0800

    [IMP]dashBoard添加版本进度后端数据 & 添加活跃冲刺后端数据

--- agile/src/app/agile/dashboard/SprintProgress/SprintProgressHome/SprintProgressHome.js
+++ agile/src/app/agile/dashboard/SprintProgress/SprintProgressHome/SprintProgressHome.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
+import { stores, axios } from 'choerodon-front-boot';
 import { Progress } from 'choerodon-ui';
 import './SprintProgressHome.scss';
 
+const { AppState } = stores;
+
 class SprintProgressHome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sprint: {},
+    };
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+  
+  getTotalDay(startDate, endDate) {
+    if (startDate !== undefined && startDate !== null && endDate !== undefined && endDate !== null) {
+      startDate = startDate.substr(0, 10);
+      endDate = endDate.substr(0, 10);
+      const aStartDate = startDate.split('-');
+      const aEndDate = endDate.split('-');
+      startDate = new Date(`${aStartDate[1]}-${aStartDate[2]}-${aStartDate[0]}`);
+      endDate = new Date(`${aEndDate[1]}-${aEndDate[2]}-${aEndDate[0]}`);
+      return (parseInt(Math.abs(startDate - endDate) / 1000 / 60 / 60 / 24) + 1);
+    }
+  }
+
+  loadData() {
+    const projectId = AppState.currentMenuType.id;
+    axios.get(`agile/v1/projects/${projectId}/sprint/active`)
+      .then((res) => {
+        this.setState({
+          sprint: res,
+        });
+      });  
+  }
+
   render() {
-    const totalDay = 12;
-    const pastDay = 8;
+    const { sprint } = this.state;
+    const totalDay = this.getTotalDay(sprint.startDate, sprint.end);
     return (
       <div className="c7n-SprintProgressHome">
-        <p className="c7n-SprintStage">8/20-8/31 迭代冲刺</p>
-        <p className="c7n-SprintPastDay">
-
-
-剩余
-          <span className="c7n-pastDay">{totalDay - pastDay}</span>
-
-
-天
-                </p>
+        <p className="c7n-SprintStage">
+          {`${sprint.startDate !== undefined && sprint.startDate !== null && sprint.startDate.substr(5, 2).replace(/\b(0+)/gi, '')}/${sprint.startDate !== undefined && sprint.startDate !== null && sprint.startDate.substr(8, 2)}-${sprint.endDate !== undefined && sprint.endDate !== undefined && sprint.endDate.substr(5, 2).replace(/\b(0+)/gi, '')}/${sprint.endDate !== undefined && sprint.endDate !== null && sprint.endDate.substr(8, 2)} ${sprint.sprintName}`}
+        </p>
+        <p className="c7n-SprintRemainDay">
+          {'剩余'}
+          <span className="c7n-remainDay">{sprint.dayRemain > 0 ? sprint.dayRemain : 0}</span>
+          {'天'}
+        </p>
         <div className="c7n-progress">
-          <Progress percent={pastDay / totalDay * 100} showInfo={false} />
-          <span className="c7n-sprintStart">8/20</span>
-          <span className="c7n-sprintEnd">8/31</span>
-
+          <Progress percent={(sprint.dayRemain > 0 ? totalDay - sprint.dayRemain : totalDay) / totalDay * 100} showInfo={false} />
+          <span className="c7n-sprintStart">{`${(sprint.startDate !== undefined && sprint.startDate !== null) && sprint.startDate.substr(5, 2).replace(/\b(0+)/gi, '')}/${sprint.startDate !== undefined && sprint.startDate !== null && sprint.startDate.substr(8, 2)}`}</span>
+          <span className="c7n-sprintEnd">{`${(sprint.endDate !== undefined && sprint.endDate !== null) && sprint.endDate.substr(5, 2).replace(/\b(0+)/gi, '')}/${(sprint.endDate !== undefined && sprint.endDate !== null) && sprint.endDate.substr(8, 2)}`}</span>
         </div>
       </div>
     );
