b88a821ccdc872c97289675b961ba62f11bd2030
laincarl <1047972853@qq.com>
2019-04-04 11:05:45 +0800

    更改项目群看板文件名

---
+++ agile/src/app/agile/containers/program/Kanban/BoardSetting/components/SwimLanePage/SwimLanePage.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { Content } from 'choerodon-front-boot';
+import { Select } from 'choerodon-ui';
+import KanbanStore from '../../../../../../stores/Program/Kanban/KanbanStore';
+
+
+const { Option } = Select;
+
+@observer
+class SwimLanePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectValue: '',
+    };
+  }
+
+  handleSave(select) {
+    const { selectValue, selectedValue } = this.state;
+    const data = {
+      // objectVersionNumber: select.objectVersionNumber,
+      boardId: select.boardId,
+      swimlaneBasedCode: selectValue || KanbanStore.getSwimLaneCode,
+    };
+    KanbanStore.axiosUpdateBoardDefault(data).then((res) => {
+      KanbanStore.setSwimLaneCode(selectedValue);
+      Choerodon.prompt('保存成功');
+    }).catch((error) => {      
+      Choerodon.prompt('保存失败');
+    });
+  }
+
+  render() {
+    const defaultSelect = KanbanStore.getBoardList.get(KanbanStore.getSelectedBoard);
+    return (
+      <Content
+        description="请在下面输入列名，选择列的类别。可以添加、删除、重新排序和重命名一个列。状态为 待处理 的特性卡片可以在项目中显示。"
+        style={{
+          padding: 0,         
+          height: '100%',
+        }}
+        link="http://v0-10.choerodon.io/zh/docs/user-guide/agile/sprint/manage-kanban/"
+      >
+        <Select
+          style={{ width: 512 }}
+          label="基础泳道在"
+          defaultValue={KanbanStore.getSwimLaneCode || 'swimlane_none'}
+          onChange={(value) => {
+            this.setState({
+              selectValue: value,
+            }, () => {
+              this.handleSave(defaultSelect);
+            });
+          }}
+        >
+          <Option value="feature">特性</Option>          
+          <Option value="swimlane_none">无</Option>
+        </Select>
+      </Content>
+    );
+  }
+}
+
+export default SwimLanePage;
