6004f8d839e94a72dfa0756f6afa3f84a64f6058
Song <jian.song01@hand-china.com>
2019-04-16 11:39:19 +0800

    [FIX]修复创建问题页面日期类型初始化值报错

--- agile/src/app/agile/components/CreateIssueNew/CreateIssue.js
+++ agile/src/app/agile/components/CreateIssueNew/CreateIssue.js
@@ -559,7 +559,9 @@ class CreateIssue extends Component {
   getFieldComponent = (field) => {
     const { form } = this.props;
     const { getFieldDecorator } = form;
-    const { defaultValue, fieldName, fieldCode } = field;
+    const {
+      defaultValue, fieldName, fieldCode, fieldType,
+    } = field;
     const {
       originIssueTypes, originPriorities, defaultPriority, storyPoints,
       edit, delta, originUsers, selectLoading, estimatedTime,
@@ -964,12 +966,26 @@ class CreateIssue extends Component {
           <FormItem label={fieldName} style={{ width: 520 }}>
             {getFieldDecorator(fieldCode, {
               rules: [{ required: true, message: `${fieldName}为必填项` }],
-              initialValue: defaultValue || undefined,
+              initialValue: this.transformValue(fieldType, defaultValue),
             })(
               this.renderField(field),
             )}
           </FormItem>
         );
+    }
+  };
+
+  transformValue = (fieldType, value) => {
+    if (value) {
+      if (fieldType === 'time' || fieldType === 'datetime') {
+        return value ? moment(value) : undefined;
+      } else if (value instanceof Array) {
+        return value.slice();
+      } else {
+        return value;
+      }
+    } else {
+      return undefined;
     }
   };
 
